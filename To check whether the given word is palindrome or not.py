1e63130c8c45bf9df82ddf3a12f072b06acab3ba
SAMIUN HANI <167081158+Samiunhani64@users.noreply.github.com>
2024-05-02 22:02:03 +0530

    Create To check whether the given word is palindrome or not.py

---
+++ To check whether the given word is palindrome or not.py
@@ -0,0 +1,9 @@
+def palindromestring(word):
+    if word == word[::-1]:
+        print("palindrome")
+    else:
+        print("not palimdrome")
+
+word=input()
+print(word)
+print(palindromestring(word))
